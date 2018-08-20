1ecaed2b15b078ccc9d00ac8a87dc63f8a917cfb
unicorn <850348048@qq.com>
2018-08-20 17:31:59 +0800

    first commit

--- README.txt
+++ README.txt
@@ -1,3 +1,5 @@
 1 资源id 的查找路径：
   app/build/generate/source/r/debug/对应包名
-2
+2 注释代码：
+   ctrl+ /         //注释代码行
+   shift+ctrl+ /   /**/注释代码行
--- app/src/main/java/com/example/administrator/blue_yatest/MainActivity.java
+++ app/src/main/java/com/example/administrator/blue_yatest/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.administrator.blue_yatest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.bluetooth.BluetoothClass;
-import com.ld.blecardlibrarydes.ble.BLEUtil;
+//import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+//import android.content.Intent;
+//import android.view.Menu;
+//import android.view.View;
+//import android.widget.ArrayAdapter;
+//import  android.widget.ListAdapter;
+  import  android.widget.ListView;
+  import android.widget.Button;
+
+import java.util.Set;   //包含了Set集合的方法
+//import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    private Button On,Off,Connect,Disconnect,Search,Sendapdu;
+    private BluetoothAdapter BA;
+    private Set<BluetoothDevice> pairedDevices;
+    private ListView lv;
+
+
+    @Override  //伪代码，表示重写
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        On  = (Button)findViewById(R.id.button1);
+        Off = (Button)findViewById(R.id.button2);
+        Connect = (Button)findViewById(R.id.button3);
     }
 }
